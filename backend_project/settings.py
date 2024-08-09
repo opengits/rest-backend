dc184c7325e7b5c37879cdfb70051b209380d27e
opengits <kamalsharma.git@gmail.com>
2024-08-09 12:14:11 +0100

    Final Commit

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -83,10 +83,15 @@ WSGI_APPLICATION = 'backend_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
